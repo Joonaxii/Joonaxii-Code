2677b09395989f4034817e03265cddbddbd50537
JOONAXII-PC\Joonaxii <Joonaxii@gmail.com>
2022-03-21 21:58:02 +0200

    Update Matrix3x3 to use a fixed float[]

--- Joonaxii/Joonaxii.MathJX/src/Matrix3x3.cs
+++ Joonaxii/Joonaxii.MathJX/src/Matrix3x3.cs
@@ -6,7 +6,7 @@ using MathS = System.Math;
 namespace Joonaxii.MathJX
 {
     [StructLayout(LayoutKind.Sequential, Size = 36)]
-    public struct Matrix3x3 
+    public unsafe struct Matrix3x3
     {
         public static Matrix3x3 identity { get; } = new Matrix3x3(1, 0, 0, 0, 1, 0, 0, 0, 1);
         public static Matrix3x3 zero { get; } = new Matrix3x3(0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -18,17 +18,7 @@ namespace Joonaxii.MathJX
         // m20 m21 m22 //
         /////////////////
 
-        private float _m00;
-        private float _m01;
-        private float _m02;
-
-        private float _m10;
-        private float _m11;
-        private float _m12;
-
-        private float _m20;
-        private float _m21;
-        private float _m22;
+        private fixed float _m[9];
 
         public float this[int r, int c]
         {
@@ -38,70 +28,42 @@ namespace Joonaxii.MathJX
 
         public float this[int i]
         {
-            get
-            {
-                switch (i)
-                {
-                    default: throw new IndexOutOfRangeException($"Invalid matrix index '{i}'");
-
-                    case 0: return _m00;
-                    case 1: return _m01;
-                    case 2: return _m02;
-                    case 3: return _m10;
-                    case 4: return _m11;
-                    case 5: return _m12;
-                    case 6: return _m20;
-                    case 7: return _m21;
-                    case 8: return _m22;
-                }
-            }
-
+            get => i < 0 | i > 8 ? 0 : _m[i];
             set
             {
-                switch (i)
-                {
-                    default: throw new IndexOutOfRangeException($"Invalid matrix index '{i}'");
-                    case 0: _m00 = value; break;
-                    case 1: _m01 = value; break;
-                    case 2: _m02 = value; break;
-                    case 3: _m10 = value; break;
-                    case 4: _m11 = value; break;
-                    case 5: _m12 = value; break;
-                    case 6: _m20 = value; break;
-                    case 7: _m21 = value; break;
-                    case 8: _m22 = value; break;
-                }
+                if(i < 0 | i > 8) { return; }
+                _m[i] = value;
             }
         }
 
         public Matrix3x3(float m00, float m01, float m02, float m10, float m11, float m12, float m20, float m21, float m22)
         {
-            _m00 = m00;
-            _m01 = m01;
-            _m02 = m02;
+            _m[0] = m00;
+            _m[1] = m01;
+            _m[2] = m02;
 
-            _m10 = m10;
-            _m11 = m11;
-            _m12 = m12;
+            _m[3] = m10;
+            _m[4] = m11;
+            _m[5] = m12;
 
-            _m20 = m20;
-            _m21 = m21;
-            _m22 = m22;
+            _m[6] = m20;
+            _m[7] = m21;
+            _m[8] = m22;
         }
 
         public Matrix3x3(Vector3 c0, Vector3 c1, Vector3 c2)
         {
-            _m00 = c0.x;
-            _m10 = c0.y;
-            _m20 = c0.z;
+            _m[0] = c0.x;
+            _m[3] = c0.y;
+            _m[6] = c0.z;
 
-            _m01 = c1.x;
-            _m11 = c1.y;
-            _m21 = c1.z;
+            _m[1] = c1.x;
+            _m[4] = c1.y;
+            _m[7] = c1.z;
 
-            _m02 = c2.x;
-            _m12 = c2.y;
-            _m22 = c2.z;
+            _m[2] = c2.x;
+            _m[5] = c2.y;
+            _m[8] = c2.z;
         }
 
         public static Matrix3x3 TRS(Vector2 point, float zRotation, Vector2 scale) => new Matrix3x3().SetTRS(point, zRotation, scale);
@@ -113,9 +75,9 @@ namespace Joonaxii.MathJX
             return this;
         }
 
-        public float Determinant() => (_m00 * Det2x2(4, 8, 5, 7)) - 
-                                      (_m01 * Det2x2(3, 8, 5, 6)) + 
-                                      (_m02 * Det2x2(3, 5, 4, 6));
+        public float Determinant() => (_m[0] * Det2x2(4, 8, 5, 7)) -
+                                      (_m[1] * Det2x2(3, 8, 5, 6)) +
+                                      (_m[2] * Det2x2(3, 5, 4, 6));
 
         private float Det2x2(int a, int b, int c, int d) => (this[a] * this[b]) - (this[c] * this[d]);
 
@@ -139,20 +101,20 @@ namespace Joonaxii.MathJX
             //  G  H  I //     //  +  -  + //
             /////////////////////////////////
 
-            float detA =  tr.Det2x2(4, 5, 7, 8) * det; //Scale:  X
+            float detA = tr.Det2x2(4, 5, 7, 8) * det; //Scale:  X
             float detB = -tr.Det2x2(3, 5, 6, 8) * det; //Rot A:  Cos(a)
-            float detC =  tr.Det2x2(3, 4, 6, 7) * det; //Rot B: -Sin(a)
+            float detC = tr.Det2x2(3, 4, 6, 7) * det; //Rot B: -Sin(a)
 
             float detD = -tr.Det2x2(1, 2, 7, 8) * det; //Scale:  Y
-            float detE =  tr.Det2x2(0, 2, 6, 8) * det; //Rot C:  Sin(a)
+            float detE = tr.Det2x2(0, 2, 6, 8) * det; //Rot C:  Sin(a)
             float detF = -tr.Det2x2(0, 1, 6, 7) * det; //Rot D:  Cos(a)
 
-            float detG =  tr.Det2x2(1, 2, 4, 5) * det; //Pos:    X
+            float detG = tr.Det2x2(1, 2, 4, 5) * det; //Pos:    X
             float detH = -tr.Det2x2(0, 2, 3, 5) * det; //Pos:    Y
-            float detI =  tr.Det2x2(0, 1, 3, 4) * det; //Rot E:  Angles
+            float detI = tr.Det2x2(0, 1, 3, 4) * det; //Rot E:  Angles
 
-            return new Matrix3x3(detA, detB, detC, 
-                                 detD, detE, detF, 
+            return new Matrix3x3(detA, detB, detC,
+                                 detD, detE, detF,
                                  detG, detH, detI);
         }
 
@@ -193,63 +155,67 @@ namespace Joonaxii.MathJX
 
         public Vector2 MultiplyPoint(Vector2 point)
         {
-            float pX = point.x * _m00;
-            float pY = point.y * _m10;
-            return new Vector2((pX * _m01 + pY * _m02) + _m20, (pY * _m12 + pX * _m11) + _m21);
+            float pX = point.x * _m[0];
+            float pY = point.y * _m[3];
+            return new Vector2((pX * _m[1] + pY * _m[2]) + _m[6], (pY * _m[5] + pX * _m[4]) + _m[7]);
         }
 
         public Vector2 MultiplyVector(Vector2 vec)
         {
-            float pX = vec.x * _m00;
-            float pY = vec.y * _m10;
-            return new Vector2((pX * _m01 + pY * _m02), (pY * _m12 + pX * _m11));
+            float pX = vec.x * _m[0];
+            float pY = vec.y * _m[3];
+            return new Vector2((pX * _m[1] + pY * _m[2]), (pY * _m[5] + pX * _m[4]));
         }
 
-        public Vector2 RotatePoint(Vector2 vec) => 
+        public Vector2 RotatePoint(Vector2 vec) =>
             new Vector2(
-            (vec.x * _m01 + vec.y * _m02), 
-            (vec.y * _m12 + vec.x * _m11));
+            (vec.x * _m[1] + vec.y * _m[2]),
+            (vec.y * _m[5] + vec.x * _m[4]));
 
-        public Vector2 MultiplyAbsVector(Vector2 vec) 
+        public Vector2 MultiplyAbsVector(Vector2 vec)
         {
-            float pX = vec.x * _m00;
-            float pY = vec.y * _m10;
-            return new Vector2((pX * MathS.Abs(_m01) + pY * MathS.Abs(_m02)),
-                        (pY * MathS.Abs(_m12) + pX * MathS.Abs(_m11)));
+            float pX = vec.x * _m[0];
+            float pY = vec.y * _m[3];
+            return new Vector2((pX * MathS.Abs(_m[1]) + pY * MathS.Abs(_m[2])),
+                        (pY * MathS.Abs(_m[5]) + pX * MathS.Abs(_m[4])));
         }
 
-        public Vector2 ScaleVector(Vector2 vec) => new Vector2(vec.x * _m00, vec.y * _m10);
-        public float Rotate(float rotation) => rotation + _m22;
+        public Vector2 ScaleVector(Vector2 vec) => new Vector2(vec.x * _m[0], vec.y * _m[3]);
+        public float Rotate(float rotation) => rotation + _m[8];
 
         public Matrix3x3 Transpose()
         {
-            return new Matrix3x3(_m00, _m10, _m20, 
-                                 _m01, _m11, _m21, 
-                                 _m02, _m12, _m22);
+            return new Matrix3x3(_m[0], _m[3], _m[6],
+                                 _m[1], _m[4], _m[7],
+                                 _m[2], _m[5], _m[8]);
         }
+
+        public Vector2 GetPosition() => new Vector2(_m[6], _m[7]);
+        public float GetRotation() => _m[8];
+        public Vector2 GetScale() => new Vector2(_m[0], _m[3]);
 
         public void SetScale(Vector2 scale)
         {
-            _m00 = scale.x;
-            _m10 = scale.y;
+            _m[0] = scale.x;
+            _m[3] = scale.y;
         }
         public void SetRotation(float zRotation)
         {
             float rads = zRotation * Maths.Deg2Rad;
-            _m01 = Maths.Cos(rads);
-            _m11 = Maths.Sin(rads);
+            _m[1] = Maths.Cos(rads);
+            _m[4] = Maths.Sin(rads);
 
-            _m02 = -_m11;
-            _m12 = _m01;
+            _m[2] = -_m[4];
+            _m[5] = _m[1];
 
-            _m22 = zRotation;
+            _m[8] = zRotation;
         }
         public void SetPosition(Vector2 point)
         {
-            _m20 = point.x;
-            _m21 = point.y;
+            _m[6] = point.x;
+            _m[7] = point.y;
         }
 
-        public override string ToString() => $"M00: {_m00}\t M01: {_m01}\t M02: {_m02}\n M10: {_m10}\t M11: {_m11}\t M12: {_m12}\n M20: {_m20}\t M21: {_m21}\t M22: {_m22}";
+        public override string ToString() => $"M00: {_m[0]}\t M01: {_m[1]}\t M02: {_m[2]}\n M10: {_m[3]}\t M11: {_m[4]}\t M12: {_m[5]}\n M20: {_m[6]}\t M21: {_m[7]}\t M22: {_m[8]}";
     }
 }
